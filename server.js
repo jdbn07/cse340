f792a37bd5725bd92b27e5faef54d86da8f1a3fb
Jim Davis <jdbn_07@hotmail.com>
2025-03-12 20:44:19 -0500

    finish homework

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
@@ -20,12 +26,6 @@ app.get("/", function (req, res) {
   res.render("index", { title: "Home" })
 })
 
-/* ***********************
- * View Engine and Templates
- *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Local Server Information
